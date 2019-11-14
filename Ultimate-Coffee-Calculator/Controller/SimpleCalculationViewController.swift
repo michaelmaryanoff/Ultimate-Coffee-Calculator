1ab9b992b2e016de3f26725ce5277667d5cfe865
MichaelMaryanoff <mmaryanoff@gmail.com>
2019-11-14 13:51:07 -0500

    Set up view and view controller

--- Ultimate-Coffee-Calculator/Controller/SimpleCalculationViewController.swift
+++ Ultimate-Coffee-Calculator/Controller/SimpleCalculationViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SimpleCalculationViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
