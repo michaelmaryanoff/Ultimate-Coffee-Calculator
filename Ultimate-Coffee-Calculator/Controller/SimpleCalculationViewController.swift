bce22d18167e78957a8559271c9d12d98bf346e2
MichaelMaryanoff <mmaryanoff@gmail.com>
2019-11-14 16:52:07 -0500

    Create working method for checking allowed characters and length

--- Ultimate-Coffee-Calculator/Controller/SimpleCalculationViewController.swift
+++ Ultimate-Coffee-Calculator/Controller/SimpleCalculationViewController.swift
@@ -28,7 +28,33 @@ extension SimpleCalculationViewController: UITextFieldDelegate {
     
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         
-        if textField == waterTextField {
+
+        
+        if customTextOutput(textField: textField, shouldChangeCharactersIn: range, replacementString: string) == false {
+            return false
+        } else {
+            return true
+        }
+        
+    }
+    
+    func customTextOutput(textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        if textField == coffeeTextField {
+            var result = true
+            guard let text = textField.text else {
+                print("there was no text")
+                return false
+            }
+            
+            let disallowedCharacterSet = NSCharacterSet(charactersIn: "0123456789.-").inverted
+            let replacementStringIsLegal = string.rangeOfCharacter(from: disallowedCharacterSet) == nil
+            result = replacementStringIsLegal
+        
+            let maxCoffeeStringLength = 4
+            let currentString: NSString = text as NSString
+            let newCoffeeString: NSString = currentString.replacingCharacters(in: range, with: string) as NSString
+            return newCoffeeString.length <= maxCoffeeStringLength && result
             
         }
         return true
