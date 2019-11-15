1c13db2965cff4c29e926cc4a2f6ec2f82e0b082
MichaelMaryanoff <mmaryanoff@gmail.com>
2019-11-15 16:43:30 -0500

    Abstract methods

--- Ultimate-Coffee-Calculator/Controller/Extensions/SimpleCalculatorViewController+Extensions.swift
+++ Ultimate-Coffee-Calculator/Controller/Extensions/SimpleCalculatorViewController+Extensions.swift
@@ -10,28 +10,34 @@ import UIKit
 
 extension SimpleCalculationViewController {
     
+    // MARK: - Lifecycle functions
     func initialSetup() {
+        
+        addDelegates()
+        addGestureRecognizer()
+        subscribeToKeyboardNotifications()
+        view.setGradientBackground(from: Colors.deepOrange, to: Colors.mellowOrange)
+    }
+    
+    // MARK: - Helper functions
+    func addDelegates() {
         coffeeTextField.delegate = self
         ratioTextField.delegate = self
         waterTextField.delegate = self
-        
-        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
-        
-        view.addGestureRecognizer(tap)
-        
-        view.setGradientBackground(from: Colors.deepOrange, to: Colors.mellowOrange)
-        
-        subscribeToKeyboardNotifications()
     }
     
+    func addGestureRecognizer() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    
+    // MARK: - Keyboard functions
     func subscribeToKeyboardNotifications() {
-        
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
     func unsubscribeFromKeyboardNotifications() {
-        
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
     }
@@ -40,7 +46,6 @@ extension SimpleCalculationViewController {
         if waterTextField.isEditing {
             view.frame.origin.y -= getKeyboardHeight(notification)
         }
-        
     }
     
     @objc func keyboardWillHide(_ notification:Notification) {
@@ -48,7 +53,6 @@ extension SimpleCalculationViewController {
     }
     
     func getKeyboardHeight(_ notification:Notification) -> CGFloat {
-        
         let userInfo = notification.userInfo
         let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue
         return keyboardSize.cgRectValue.height
